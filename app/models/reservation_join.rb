ae68e4c535dbe51e91f5a145c2443b6aeeaaf730
Elif Sezgin <elifmsezgin@gmail.com>
2017-03-11 14:18:14 -0800

    Added Reservations and ReservationJoins tables to db

---
+++ app/models/reservation_join.rb
@@ -0,0 +1,2 @@
+class ReservationJoin < ApplicationRecord
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,31 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170311205943) do
+ActiveRecord::Schema.define(version: 20170311221424) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "reservation_joins", force: :cascade do |t|
+    t.integer  "reservation_id", null: false
+    t.integer  "user_id",        null: false
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
+    t.index ["reservation_id"], name: "index_reservation_joins_on_reservation_id", using: :btree
+    t.index ["user_id", "reservation_id"], name: "index_reservation_joins_on_user_id_and_reservation_id", unique: true, using: :btree
+    t.index ["user_id"], name: "index_reservation_joins_on_user_id", using: :btree
+  end
+
+  create_table "reservations", force: :cascade do |t|
+    t.string   "name",       null: false
+    t.string   "address",    null: false
+    t.string   "state",      null: false
+    t.string   "city",       null: false
+    t.string   "zip",        null: false
+    t.time     "time",       null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "image_url",     null: false
