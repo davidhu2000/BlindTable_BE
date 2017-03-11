79ff9c0a4321f9c3fbd1f4b6cda4a9e65d1910c6
Elif Sezgin <elifmsezgin@gmail.com>
2017-03-11 13:41:21 -0800

    Added session controllers

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,2 +1,25 @@
 class Api::SessionsController < ApplicationController
+  def create
+    @user = User.find_by_phone_number(session_params[:phone_number])
+    if @user
+      login(@user)
+      render 'api/users/show'
+    else
+      render json: ['Phone number not found'], status: 404
+    end
+  end
+
+  def destroy
+    if current_user
+      logout
+      render json: { currentUser: nil, errors: [] }
+    else
+      render json: ['There is no user logged in'], status: 404
+    end
+  end
+
+  private
+  def session_params
+    params.require(:user).permit(:phone_number)
+  end
 end
